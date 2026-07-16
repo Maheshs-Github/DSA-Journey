095d5ac384ac0a680265ce991961a46cc12461e8
Mahesh Mane <maheshmane9075@gmail.com>
2026-07-16 10:41:35 +0530

    adding the chages of BS , floor ceil of the number and understading the lower and upper bound for the LAst and first occurance

---
+++ Binary_Search/FirstAndLastOccurance.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+
+public class FirstAndLastOccurance {
+
+  // let's see brute force 
+  // public static int[] fLOccurance(int[] a, int key) {
+  //   int left = 0, right = a.length - 1, leftVal = -1, rightVal = -1;
+  //   while (left <= right) {
+  //     if (a[left] == key && leftVal == -1)
+  //       leftVal = left;
+  //     if (a[right] == key && rightVal == -1)
+  //       rightVal = right;
+  //     left++;
+  //     right--;
+  //   }
+  //   if (leftVal != -1 && rightVal != -1)
+  //     return new int[] { leftVal, rightVal };
+  //   if (leftVal != -1 && rightVal == -1)
+  //     return new int[] { leftVal, leftVal };
+  
+  //   if (leftVal == -1 && rightVal != -1)
+  //     return new int[] { rightVal, rightVal };
+  //   else
+  //     return new int[] { -1,-1 };
+
+  // }
+
+  // now let's see how we can do with the BS 
+
+  // public static int[] fLOccurance(int[] a, int key) {
+//   public static int[] fLOccurance(int[] a, int key) {
+//     return search(a,0,a.length-1,key);
+// }
+// public static int[] search(int[] a,int lb,int ub,int key){
+//   int[] ans=new int[2];
+//   int i=0;
+//   if(lb>ub){
+//     ans[i]=lb;
+//     return ans;
+//   }
+//     int mid=Math.floorDiv(lb+ub, 2);
+//     System.out.println("mid: "+mid);
+//     if(a[mid]==key){
+//       ans[i]=mid;
+//       i++;
+//     }
+//       // System.out.println("ans: "+mid);
+//     if (a[mid]<=key)
+//       return search(a, mid+1, ub, key);
+//     else if(a[mid]>=key)
+//       return search(a, lb, mid-1, key);
+
+//     return new int[] {-1,-1};
+//   }
+
+  // let's see how we can get the first occurance then 
+    public static int fLOccurance(int[] a, int key) {
+    return search(a,0,a.length-1,key);
+}
+public static int search(int[] a,int lb,int ub,int key){
+  if(lb>ub){
+    return lb;
+  }
+    int mid=Math.floorDiv(lb+ub, 2);
+    // if (a[mid]>=key)
+    //   return search(a, lb, mid-1, key);
+    // else 
+    //   return search(a, mid+1, ub, key);
+    // i will assign the same fun for bot if else but order should be revsree
+        if (a[mid]<=key)
+          return search(a, mid+1, ub, key);
+        else 
+          return search(a, lb, mid-1, key);
+
+    // return -1;
+  }
+
+// }
+
+
+  public static void main(String[] args) {
+    // int[] ans= fLOccurance(new int[] {1,2,2,2,3},2); //1,3
+    // int[] ans= fLOccurance(new int[] {5,7,7,8,8,10},6); //-1,-1
+    // int[] ans= fLOccurance(new int[] {4,4,4,4,4},4); //0,4
+
+
+    // int[] ans=fLOccurance(new int[] {4,4,4,4,4},4); //0,4
+
+    // for low bound
+    // int ans=fLOccurance(new int[] {4,4,4,4,4},4); //0
+    // int ans=fLOccurance(new int[] {5,7,7,8,8,10},8); //3
+    int ans= fLOccurance(new int[] {1,2,2,2,3},2); //1
+
+    System.out.println("ans: "+ans);
+    // System.out.println("ans: "+Arrays.toString(ans));
+  }
+
+}
+
+
+// how chnaging the a[mid]<=key  condition gives the lower bound and upper bound 
+
--- Binary_Search/FloorCeil.java
+++ Binary_Search/FloorCeil.java
@@ -1,25 +1,56 @@
+import java.util.Arrays;
+
 public class FloorCeil {
-  public static void floCel(int[] a,int key){
-       search(a,0,a.length-1,key);
+  public static int[] floCel(int[] a, int key) {
+    return search(a, 0, a.length - 1, key);
   }
-  public static void search(int[] a,int low,int high,int key){
-    if(low>high)
-    {
-      System.out.println("[ "+high +low+" ]");
-      return;
+
+  public static int[] search(int[] a, int low, int high, int key) {
+    if (high < 0)
+      return new int[] { -1,a[low] };
+    System.out.println("len: " + a[a.length - 1]);
+    if (low > a.length - 1)
+      return new int[] { a[high], -1 };
+
+    // System.out.println("hello");
+    if (low > high) {
+      // System.out.println("[ "+high +" "+ low+" ]");
+      return new int[] { a[high], a[low] };
     }
-    int mid=Math.floorDiv(low+high, 2);
-    if(a[mid]==key){
-      System.out.println("[ "+mid +mid+" ]");
-      return;
+    int mid = Math.floorDiv(low + high, 2);
+    System.out.println("high: " + high + " low: " + low + " mid: " + mid + " a[mid]" + a[mid]);
+    if (a[mid] == key) {
+      // System.out.println("[ "+mid +mid+" ]");
+      return new int[] { a[mid], a[mid] };
     }
-     if(a[mid]>=key)
-      search(a, mid+1, high, key);
+    if (a[mid] >= key)
+      return search(a, low, mid - 1, key);
     else
-      search(a, low, mid-1, key);
+      return search(a, mid + 1, high, key);
   }
+
   public static void main(String[] args) {
-    floCel(new int[] {3,5,8,9,15,19}, 9);
+    // int[] ans=floCel(new int[] {3,5,8,15,19}, 9); //8,15
+    // int[] ans = floCel(new int[] { 3, 5, 8, 15, 19 }, 1); // -1,3
+    int[] ans = floCel(new int[] { 3, 5, 8, 15, 19 }, 21); // -1,3
+
+    System.out.println("Arrays : " + Arrays.toString(ans));
   }
-  
+
 }
+
+// okay simply what we had to do here 
+    // int[] ans=floCel(new int[] {3,5,8,15,19}, 9); //8,15
+    // int[] ans=floCel(new int[] {3,5,8,15,19}, 8); //8,8
+    // int[] ans = floCel(new int[] { 3, 5, 8, 15, 19 }, 1); // -1,3
+    // int[] ans = floCel(new int[] { 3, 5, 8, 15, 19 }, 21); // -1,3
+
+    // first 2 example are simple , if we find the elment just return it teice , if we do not get the specific elment just negibours or adjecent 9-> 8,15 
+    // let's see our example {3,5,8,15,19} , key =9
+    //  first call low=0 , high=4 , mid=4/2=2 a[mid]=8
+    // key>mid, so low=3, high=4, mid=6/2=3 a[mid]=15
+    // key<mid , so low=3, high=3 mid=3 a[mid]=15
+    // key<mid , so low =3 , high=2 , so low > high RETURN a[high],a[low]
+
+
+    // let's the dry run of this program on edge cases
